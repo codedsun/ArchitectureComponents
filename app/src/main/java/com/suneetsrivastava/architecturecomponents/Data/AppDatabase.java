787525bcf8e49db21ef696d7e69b5e179ce14c49
Suneet Srivastava <suneetbond91@gmail.com>
2018-01-09 22:16:54 +0530

    ROOM implemented

---
+++ app/src/main/java/com/suneetsrivastava/architecturecomponents/Data/AppDatabase.java
@@ -0,0 +1,23 @@
+package com.suneetsrivastava.architecturecomponents.Data;
+
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+
+/**
+ * Created by suneetsrivastava on 09/01/18.
+ */
+@Database(entities = {DatabaseModel.class}, version = 1)
+public abstract class AppDatabase extends RoomDatabase {
+
+    private static AppDatabase INSTANCE;
+
+    public static AppDatabase getInstacnce(Context context){
+        if(INSTANCE == null){
+            Room.databaseBuilder(context, AppDatabase.class, "todo").build();
+        }
+        return INSTANCE;
+    }
+}
--- app/src/main/java/com/suneetsrivastava/architecturecomponents/Data/DAO.java
+++ app/src/main/java/com/suneetsrivastava/architecturecomponents/Data/DAO.java
@@ -1,13 +1,47 @@
 package com.suneetsrivastava.architecturecomponents.Data;
 
+import android.arch.lifecycle.LiveData;
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
 import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.TypeConverter;
+import android.arch.persistence.room.TypeConverters;
+
+import java.util.List;
 
 /**
  * Created by suneetsrivastava on 09/01/18.
  */
 
+@Dao
+@TypeConverters(DateConverter.class)
 public interface DAO {
 
-    @Insert
-    void insert()
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insert(DatabaseModel databaseModels);
+
+    @Query("Select *from DatabaseModel")
+    LiveData<List<DatabaseModel>> getAllTask();
+
+    @Query("Select * from DatabaseModel where id = :id")
+    LiveData<DatabaseModel> getTaskById(int id);
+
+    @Query("Select * from DatabaseModel where tag = :tag")
+    LiveData<List<DatabaseModel>> getTaskByTag(String tag);
+
+    @Query("Select * from DatabaseModel where priority = :priority")
+    LiveData<List<DatabaseModel>> getTaskByPriority(String priority);
+
+    @Query("Select * from DatabaseModel where isTaskDone = :isDone")
+    LiveData<List<DatabaseModel>> getTaskByStatus(Boolean isDone);
+
+    @Query("Delete from DatabaseModel where id = :id")
+    void deleteById(int id);
+
+    @Delete
+    void delete(DatabaseModel databaseModel);
+
+
 }
